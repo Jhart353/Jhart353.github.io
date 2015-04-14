83a29aad31b99055ab2bed1873df11169a6d1155
Justin Hartz <Jhart353@live.kutztown.edu>
2015-04-14 10:44:20 -0400

    Update style and feel of webpage
    Updated every file to contain nav bar
    Added functionality and changed wallpaper

---
+++ jsmaster.js
@@ -0,0 +1,12 @@
+function createNavbar(){
+	document.write("<div class='nav-bar'></div>");
+	document.write("<table>");
+	document.write("<tr>");
+	document.write("<td><a href='index.html'>Home</a></td>");
+	document.write("<td><a href='about.html'>About</a></td>");
+	document.write("<td><a href='languages.html'>Languages</a></td>");
+	document.write("<td><a href='projects.html'>Projects</a></td>");
+	document.write("</tr>");
+	document.write("</table>");
+	document.write("</div>");
+}
