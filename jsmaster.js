4db8a4faae62cc2eeadb7be949b6a3bb20f85844
Justin Hartz <Jhart353@live.kutztown.edu>
2015-04-15 08:21:43 -0400

    Update KUVT Project

--- jsmaster.js
+++ jsmaster.js
@@ -1,6 +1,6 @@
 function createNavbar(){
 	document.write("<div class='nav-bar'></div>");
-	document.write("<table>");
+	document.write("<table class='nav-table'>");
 	document.write("<tr>");
 	document.write("<td><a href='index.html'>Home</a></td>");
 	document.write("<td><a href='about.html'>About</a></td>");
